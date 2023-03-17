378e3c10a6328451cad9d4842b215da6f8b56f98
Teo <90730116+C-Teo@users.noreply.github.com>
2023-03-16 21:35:54 -0400

    Constructors for all the Classes

---
+++ creddit-FE/src/Customer.js
@@ -0,0 +1,7 @@
+class Card {
+    constructor(username,email) {
+        this.username = username;
+        this.email = email;
+        this.accounts = [];
+    }
+}
---
+++ creddit-FE/src/DebitCard.js
@@ -0,0 +1,5 @@
+class DebitCard extends Card {
+    constructor(identifier, expiry_date, account, balance=0) {
+        super(identifier, expiry_date, account, balance);
+    }
+}
