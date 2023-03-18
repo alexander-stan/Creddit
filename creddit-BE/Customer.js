307dfc8fcf15cf21c164f7dc951eb0ea6e0a3a42
Teo <90730116+C-Teo@users.noreply.github.com>
2023-03-18 13:59:21 -0400

    Moved filed to a Backend Folder

--- creddit-BE/Customer.js
+++ creddit-BE/Customer.js
@@ -1,4 +1,5 @@
-class Card {
+class Customer {
+    // This object was designed incorrectly, I'll look over the design before editing
     constructor(username,email) {
         this.username = username;
         this.email = email;
