1514da81795ff0e422f3668ccc19051bead808a8
Teo <90730116+C-Teo@users.noreply.github.com>
2023-03-25 16:23:01 -0400

    Lowercase Folders

--- creddit-BE/Test/Test.js
+++ creddit-BE/Test/Test.js
@@ -1,7 +1,7 @@
-import Account from "../Classes/Account.js"
-import Customer from "../Classes/Customer.js"
-import DebitCard from "../Classes/DebitCard.js"
-import CreditCard from "../Classes/CreditCard.js"
+import Account from "../classes/Account.js"
+import Customer from "../classes/Customer.js"
+import DebitCard from "../classes/DebitCard.js"
+import CreditCard from "../classes/CreditCard.js"
 
 // Test Case Function
 export default function runTestCases(bank) {
