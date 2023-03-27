669492797b9e787e3f47f7c81770eacce74c5598
Alexander Stan <118400920+alexander-stan@users.noreply.github.com>
2023-03-27 17:14:07 -0400

    Backend Linked to FrontEnd for Dashboard

--- creddit-BE/Classes/Account.js
+++ creddit-BE/Classes/Account.js
@@ -30,4 +30,25 @@ export default class Account {
     setCustomer(customer) {
         this.customer = customer;
     }
+
+    getCardByNumber(num) {
+
+        let accCard = null;
+
+        for (let j = 0; j < this.cards.length; j++) {
+            if (this.cards[j].getIdentifier() == num){
+                accCard = this.cards[j];
+            }
+        }
+
+        if (accCard == null){ 
+            if (this.access_card.getIdentifier() == num){
+                accCard = this.access_card;
+            }
+        }
+        if (accCard != null) {
+            return accCard;
+        }
+        return null;
+    }
 }
